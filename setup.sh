cf1528c14d34b00c1662b21f2d93a5475f9e5aa6
Sonny <sonnysonic@gmail.com>
2023-01-02 16:58:20 +0000

    please be the final fix for this

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
-
 mkdir data
 cd data
-wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
+wget --no-check-certificate http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
 mkdir bin
 wget https://archive.org/download/MSCoco2014/train2014.zip
 unzip -q train2014.zip
